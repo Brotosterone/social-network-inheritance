94074fac021c834f77801f5fb3483b234095a99d
Bob LePera <blepera91@gmail.com>
2018-07-25 20:15:14 +0000

    Project #4 final

--- social_network/posts.py
+++ social_network/posts.py
@@ -1,33 +1,62 @@
 from datetime import datetime
 
 
-# class Post(object):
-#     def __init__(self, text, timestamp=None):
-#         pass
-#
-#     def set_user(self, user):
-#         pass
+class Post(object):
+    def __init__(self, text, timestamp=None):
+        self.text =  text
+        self.timestamp = timestamp or datetime.utcnow()
+        self.timestamp = self.timestamp.strftime("%A, %b %d, %Y")
+        self.user = None
+        
+    def set_user(self, user):
+        self.user = user
+    
+    def __repr__(self):
+        return 'Post({}{})'.format(self.text,self.timestamp)
+   
+    # def __eq__(self,other):
+    #     return self.timestamp == other.timestamp
+        
+    # def __ne__(self,other):
+    #     return not self.timestamp == other.timestamp
+        
+    # def __lt__(self,other):
+    #     return self.timestamp < other.timestamp
+        
+    # def __lte__(self,other):
+    #     return self.timestamp <= other.timestamp
+        
+    # def __gt__(self,other):
+    #     return self.timestamp > other.timestamp
+    
+    # def __gte__(self,other):
+    #     return self.timestamp >= other.timestamp    
+    
+        
+class TextPost(Post):  # Inherit properly
+    def __init__(self, text, timestamp=None):
+        super(TextPost, self).__init__(text,timestamp)
+
+    def __str__(self):
+        return '@{} {}: "{}"\n\t{}'.format(self.user.first_name,self.user.last_name,self.text,self.timestamp)
 
 
-# class TextPost(...):  # Inherit properly
-#     def __init__(self, text, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+class PicturePost(Post):  # Inherit properly
+    def __init__(self, text, image_url, timestamp=None):
+        super(PicturePost, self).__init__(text,timestamp)
+        self.image_url = image_url
+        
+    def __str__(self):
+         return '@{} {}: "{}"\n\t{}\n\t{}'.format(self.user.first_name,self.user.last_name,self.text,self.image_url,self.timestamp)
 
 
-# class PicturePost(...):  # Inherit properly
-#     def __init__(self, text, image_url, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
 
+class CheckInPost(Post):  # Inherit properly
+    def __init__(self, text, latitude, longitude, timestamp=None):
+        super(CheckInPost, self).__init__(text, timestamp)
+        self.latitude = latitude
+        self.longitude = longitude
 
-# class CheckInPost(...):  # Inherit properly
-#     def __init__(self, text, latitude, longitude, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+    def __str__(self):
+         return '@{} Checked In: "{}"\n\t{}, {}\n\t{}'.format(self.user.first_name,self.text,self.latitude,self.longitude,self.timestamp)
+
